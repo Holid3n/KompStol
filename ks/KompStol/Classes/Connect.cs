8b5089d2df7487e198c42ba5de199425128e37d1
Holid3n <fomenko.denis.p@gmail.com>
2023-12-16 00:37:42 +0300

    first commit

---
+++ ks/KompStol/Classes/Connect.cs
@@ -0,0 +1,14 @@
+﻿using KompStol.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompStol.Classes
+{
+    class Connect
+    {
+        public static KompStolEntities modeldb;
+    }
+}
